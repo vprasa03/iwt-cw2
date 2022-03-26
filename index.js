4b45cf6b974ae589648fe74c690aaa07a8e19856
Vignesh Prasad <vprasa03@student.bbk.ac.uk>
2022-03-26 00:21:36 +0000

    Add js - handle form submission

---
+++ index.js
@@ -0,0 +1,23 @@
+const formatAposAndQuote = (str) =>
+	str.replaceAll("'", "&apos;").replaceAll('"', "&quote");
+
+const app = document.querySelector("#oscar-app");
+const form = document.querySelector("#oscar-form");
+
+// Js enabled, show page contents
+app.classList.remove("hidden");
+
+form.addEventListener("submit", (e) => {
+	// Prevent default redirect
+	e.preventDefault();
+
+	// GET request to fetch results
+	fetch(`${e.target.action}?key=val&two=3`, { method: "GET" })
+		.then((res) => res.text())
+		.then((res) => {
+			const results = document.querySelector("#results");
+
+			// Display results
+			results.innerHTML = res;
+		});
+});
