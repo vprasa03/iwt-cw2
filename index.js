124f34ba7fe97197c57a7105a3a36f82ad961935
Vignesh Prasad <vprasa03@student.bbk.ac.uk>
2022-03-26 02:26:10 +0000

    Add FormProcessor class

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 const app = document.querySelector("#oscar-app");
-const form = document.querySelector("#oscar-form");
+// const form = document.querySelector("#oscar-form");
 
 // Js enabled, show page contents
 app.classList.remove("hidden");
 
-form.addEventListener("submit", (e) => {
-	// Prevent default redirect
-	e.preventDefault();
+/**
+ * Process form and display results
+ */
+class FormProcessor {
+	/**
+	 * Constructor
+	 * @param {string} formID id of the form element to process
+	 * @param {string} resultsID id of the element where results are to be displayed
+	 */
+	constructor(formID, resultsID) {
+		// Find #{formID}
+		this.form = document.querySelector(formID);
+		// Find #{resultsID}
+		this.displayResults = document.querySelector(resultsID);
 
-	// GET request to fetch results
-	fetch(`${e.target.action}?key=val&two=3&three='&four="`, { method: "GET" })
-		.then((res) => res.text())
-		.then((res) => {
-			const results = document.querySelector("#results");
+		// Add "submit" event listener
+		this.form.addEventListener("submit", this.onSubmit);
+	}
 
-			// Display results
-			results.innerHTML = res;
-		});
-});
+	onSubmit = (event) => {
+		// Prevent default redirect
+		event.preventDefault();
+
+		// GET request to fetch results
+		fetch(`${event.target.action}?year=9&category=port&info='`, {
+			method: "GET",
+		})
+			.then((res) => res.text())
+			.then((res) => {
+				// Display results
+				this.displayResults.innerHTML = res;
+			})
+			.catch((err) => console.error("ERROR: ", err));
+	};
+}
+
+const form = new FormProcessor("#oscar-form", "#results");
